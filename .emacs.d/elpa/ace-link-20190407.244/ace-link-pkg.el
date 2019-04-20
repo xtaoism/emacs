0735e41e8cbc1237e2262ac0e9341bca28b1907e
xtaoism <99636941@qq.com>
2019-04-20 11:58:31 +0800

    add .emacs.d/elpa for spacemacs

---
+++ .emacs.d/elpa/ace-link-20190407.244/ace-link-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-link" "20190407.244" "Quickly follow links" '((avy "0.4.0")) :commit "30937c6fe1ffe016dc0387f3060f67eb8b5d229a" :keywords '("convenience" "links" "avy") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-link")
---
+++ .emacs.d/elpa/define-word-20190128.1559/define-word-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "define-word" "20190128.1559" "display the definition of word at point." '((emacs "24.3")) :commit "df1118858fae492cf476a963c1174bfada141c7f" :keywords '("dictionary" "convenience") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/define-word")
